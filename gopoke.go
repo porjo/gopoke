b1c9f994e5fb60bf7ba72795eafeef99048ef758
Ian Bishop <ianbishop@pace7.com>
2015-03-25 09:05:34 +1000

    Updates

--- gopoke.go
+++ gopoke.go
@@ -12,10 +12,12 @@ var suits = [...]string{"Diamonds", "Spades", "Hearts", "Clubs"}
 var suitVals = [...]int{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
 
 type Game struct {
-	Action  chan Action
-	Pot     int
-	players []Player
-	cards   []Card
+	Action      chan Action
+	Pot         int
+	MiddleCards []Card
+	players     []Player
+	cards       []Card
+	round       int
 }
 
 type Action interface {
@@ -44,51 +46,69 @@ type Card struct {
 }
 
 func init() {
-
 	rand.Seed(time.Now().UTC().UnixNano())
 }
 
 func NewGame() *Game {
-	game := &Game{}
+	var card Card
+	g := &Game{}
 
 	for _, suit := range suits {
 		for _, val := range suitVals {
-			card := Card{Suit: suit, Value: val}
-			game.cards = append(game.cards, card)
+			card = Card{Suit: suit, Value: val}
+			g.cards = append(g.cards, card)
 		}
 	}
 
 	var shuffled []Card
-	for _, idx := range rand.Perm(len(game.cards)) {
-		shuffled = append(shuffled, game.cards[idx])
+	for _, idx := range rand.Perm(len(g.cards)) {
+		shuffled = append(shuffled, g.cards[idx])
 	}
 
-	game.cards = shuffled
+	g.cards = shuffled
 
-	go game.gameRoutine()
+	// 3 cards into the middle
+	for i := 0; i < 3; i++ {
+		card, g.cards = g.cards[len(g.cards)-1], g.cards[:len(g.cards)-1]
+		g.MiddleCards = append(g.MiddleCards, card)
+	}
 
-	return game
+	go g.gameRoutine()
+
+	return g
 }
 
 func (g *Game) gameRoutine() error {
 
+	// start game
+	select {
+	case a := <-g.Action:
+		fmt.Printf("game action %v\n", a)
+	}
 	return nil
 }
 
 func (g *Game) NewRound(players []Player) ([]Player, error) {
 	var playersOut []Player
 	var card Card
+	if g.round > 3 {
+		return nil, fmt.Errorf("last round has been played, game over")
+	}
+	// first card
 	for _, p := range players {
-		if p.Chips == 0 {
-			return nil, fmt.Errorf("player %s chips cannot be $0", p.Name)
-		}
+		p.Cards = make([]Card, 0)
 		card, g.cards = g.cards[len(g.cards)-1], g.cards[:len(g.cards)-1]
 		p.Cards = append(p.Cards, card)
+	}
+
+	// second card
+	for _, p := range players {
 		card, g.cards = g.cards[len(g.cards)-1], g.cards[:len(g.cards)-1]
 		p.Cards = append(p.Cards, card)
 		playersOut = append(playersOut, p)
 	}
 	g.players = playersOut
+	g.round++
 	return playersOut, nil
 }
 
