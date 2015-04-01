8c5941fb395e80e325c74a728dac7f0dfdfa2fa2
Ian Bishop <ianbishop@pace7.com>
2015-04-01 21:42:04 +1000

    updates

--- gopoke.go
+++ gopoke.go
@@ -13,11 +13,14 @@ var suitVals = [...]int{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
 
 type Action string
 
+const Bet Action = "bet"
 const Check Action = "check"
 const Fold Action = "fold"
 const Raise Action = "raise"
 const Call Action = "call"
 const Allin Action = "All in"
+
+const StartingChips = 50
 
 type Game struct {
 	PlayerPlay  chan Play
@@ -35,6 +38,7 @@ type Player struct {
 	GamePlay chan Play
 	game     *Game
 	id       uuid.UUID
+	folded   bool
 }
 
 type Play struct {
@@ -45,8 +49,9 @@ type Play struct {
 }
 
 type Round struct {
-	highbet int
-	pot     int
+	highbet   int
+	pot       int
+	dealerIdx int
 }
 
 type Card struct {
@@ -80,81 +85,50 @@ func NewGame() *Game {
 
 	g.cards = shuffled
 
-	// 3 cards into the middle
-	for i := 0; i < 3; i++ {
-		card, g.cards = g.cards[len(g.cards)-1], g.cards[:len(g.cards)-1]
-		g.middlecards = append(g.middlecards, card)
-	}
-
 	return g
 }
 
 func (g *Game) gameRoutine() {
 	var round Round
+	var winner *Player
+	var nextPlay Play
+	round.dealerIdx = 0
 	// start game
 	fmt.Printf("Entering game routine\n")
 	fmt.Printf("------ Round %d -------\n", g.round)
 	// signal first player
-	var next int
 	play := Play{}
 	play.ValidActions = []Action{Check, Fold}
-	fmt.Printf("game notify next player, %s\n", g.players[next].name)
-	g.players[next].GamePlay <- play
+	fmt.Printf("game notify next player, %s\n", g.players[1].name)
+	g.players[1].GamePlay <- play
 	for {
 		select {
 		case play = <-g.PlayerPlay:
-
-			switch play.Action {
-			case Check:
-				// can't check on non-zero high bet
-				if round.highbet != 0 {
-					play.Action = Fold
-					close(play.Player.GamePlay)
-					break
-				}
-
-				// do nothing
-			case Fold:
-				// remove player
-				close(play.Player.GamePlay)
+			fmt.Printf("game read play %+v\n", play)
+			if play.Player.folded {
+				fmt.Printf("game: folded player %s tried to play!? Ignoring\n", play.Player.name)
 				break
-			case Raise:
-
-				if play.Amount > round.highbet {
-					round.highbet = play.Amount
-				} else { // not a raise
-					if play.Player.chips >= round.highbet {
-						if round.highbet == 0 {
-							play.Action = Check
-						} else {
-							play.Action = Call
-						}
-						play.Amount = round.highbet
-					} else {
-						play.Action = Fold
-						close(play.Player.GamePlay)
-						break
-					}
-				}
-				play.Player.chips -= play.Amount
-				if play.Player.chips == 0 {
-					play.Action = Allin
-				}
-				round.pot += play.Amount
-			case Call:
-				if round.highbet == 0 {
-					play.Action = Check
-				} else {
-					play.Player.chips -= round.highbet
-				}
-				if play.Player.chips == 0 {
-					play.Action = Allin
-				}
-				round.pot += round.highbet
-			case Allin:
 			}
 
-			fmt.Printf("game: play %s FROM %s\n", play.Action, play.Player.name)
+			fold := adjustPlay(&play, &round)
+
+			if fold {
+				count := 0
+				for i, p := range g.players {
+					if p.folded {
+						count++
+					}
+					if count == len(g.players)-1 {
+						// All other players folded, winner by default
+						fmt.Printf("game: %s is the winner by default\n", play.Player.name)
+						winner = &g.players[i]
+						goto done
+					}
+				}
+				play.Player.folded = true
+			}
+
+			fmt.Printf("game: play %s FROM %s, amount %d\n", play.Action, play.Player.name, play.Amount)
 
 			// broadcast last action to all other players
 			for i, p := range g.players {
@@ -166,30 +140,133 @@ func (g *Game) gameRoutine() {
 
 			}
 
-			next++
-			if next == len(g.players) {
-				next = 0
-				fmt.Printf("r:pot %d, g:pot %d\n", round.pot, g.pot)
-				g.pot += round.pot
-				round = Round{}
-				g.round++
-				if g.round > 3 {
-					goto done
+			// find this player
+			next := 0
+			for i, p := range g.players {
+				if uuid.Equal(p.id, play.Player.id) {
+					next = i
 				}
-				fmt.Printf("------ Round %d -------\n", g.round)
+			}
+
+			if next == round.dealerIdx {
+				// call/check finishes round
+				if play.Action == Call || play.Action == Check {
+					goto endround
+				}
+			}
+
+			// find next player
+			for {
+				next = (next + 1) % len(g.players)
+				if !g.players[next].folded {
+					break
+				}
+				if next == round.dealerIdx {
+					// call/check finishes round
+					if play.Action == Call || play.Action == Check {
+						goto endround
+					}
+				}
 			}
 
 			// notify next player
-			nextplay := Play{}
-			nextplay.ValidActions = []Action{Check, Fold}
-			fmt.Printf("game notify next player, %s\n", g.players[next].name)
-			g.players[next].GamePlay <- nextplay
+			nextPlay = Play{}
 
+			nextPlay.ValidActions = []Action{Fold, Allin}
+			if g.players[next].chips >= round.highbet {
+				nextPlay.ValidActions = append(nextPlay.ValidActions, Raise)
+				if round.highbet == 0 {
+					nextPlay.ValidActions = append(nextPlay.ValidActions, Check)
+				} else {
+					nextPlay.ValidActions = append(nextPlay.ValidActions, Call)
+				}
+			}
+
+			fmt.Printf("game notify next player, %s\n", g.players[next].name)
+			g.players[next].GamePlay <- nextPlay
+
+		endround:
+			round.dealerIdx = (round.dealerIdx + 1) % len(g.players)
+			fmt.Printf("r:pot %d, g:pot %d\n", round.pot, g.pot)
+			g.pot += round.pot
+			round = Round{}
+			g.round++
+			if g.round > 3 {
+				goto done
+			}
+
+			var card Card
+			if g.round == 1 {
+				// burn
+				_, g.cards = g.cards[len(g.cards)-1], g.cards[:len(g.cards)-1]
+				// the flop
+				for i := 0; i < 3; i++ {
+					card, g.cards = g.cards[len(g.cards)-1], g.cards[:len(g.cards)-1]
+					g.middlecards = append(g.middlecards, card)
+				}
+			}
+
+			// burn
+			_, g.cards = g.cards[len(g.cards)-1], g.cards[:len(g.cards)-1]
+			// turn/river
+			card, g.cards = g.cards[len(g.cards)-1], g.cards[:len(g.cards)-1]
+			g.middlecards = append(g.middlecards, card)
+			fmt.Printf("------ Round %d -------\n", g.round)
+
+			// First play of new round
+			nextPlay = Play{}
+
+			nextPlay.ValidActions = []Action{Fold, Allin, Check, Bet}
+			fmt.Printf("game notify next player, %s\n", g.players[round.dealerIdx].name)
+			g.players[round.dealerIdx].GamePlay <- nextPlay
 		}
 	}
 
 done:
+
+	// work out the winner
+	if winner == nil {
+
+	}
+	fmt.Printf("Winner is %v\n", winner)
 	fmt.Printf("Ending game routine\n")
+}
+
+func adjustPlay(play *Play, round *Round) (fold bool) {
+	if play.Action == Fold {
+		return true
+	}
+	if play.Amount > play.Player.chips {
+		play.Amount = play.Player.chips
+	}
+
+	fmt.Printf("game2 read play %+v, player %+v\n", play, play.Player)
+	if play.Amount == 0 {
+		if play.Action != Fold {
+			play.Action = Check
+			if round.highbet > 0 {
+				play.Action = Fold
+				return true
+			}
+		}
+	} else if play.Amount == play.Player.chips {
+		play.Action = Allin
+
+	} else if play.Amount == round.highbet {
+		play.Action = Call
+
+	} else if play.Amount > round.highbet {
+		if round.highbet == 0 {
+			play.Action = Bet
+		} else {
+
+			play.Action = Raise
+		}
+		round.highbet = play.Amount
+	}
+	play.Player.chips -= play.Amount
+	round.pot += play.Amount
+	return false
 }
 
 func (g *Game) Start() ([]Player, error) {
@@ -220,6 +297,7 @@ func (g *Game) Start() ([]Player, error) {
 
 func (g *Game) NewPlayer(name string) error {
 	p := Player{name: name}
+	p.chips = StartingChips
 	p.GamePlay = make(chan Play)
 	p.id = uuid.NewRandom()
 	g.players = append(g.players, p)
--- test/main.go
+++ test/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 
@@ -14,6 +15,8 @@ var wg sync.WaitGroup
 func main() {
 	var err error
 	var players []gopoke.Player
+
+	rand.Seed(time.Now().UTC().UnixNano())
 
 	game = gopoke.NewGame()
 
@@ -47,16 +50,19 @@ func playerRoutine(p *gopoke.Player) {
 	for {
 		select {
 		case play := <-p.GamePlay:
+
 			if play.Player != nil {
 				fmt.Printf("%s: receive game play %s (%s)\n", p.Name(), play.Action, play.Player.Name())
 			} else {
 				fmt.Printf("%s: receive notify my turn\n", p.Name())
 			}
 			if len(play.ValidActions) > 0 {
+				idxs := rand.Perm(len(play.ValidActions))
+
 				newplay := gopoke.Play{}
 				newplay.Player = p
-				newplay.Action = gopoke.Raise
-				newplay.Amount = 5
+				newplay.Action = play.ValidActions[idxs[0]]
+				newplay.Amount = 15
 				fmt.Printf("%s: sending play %s\n", p.Name(), newplay.Action)
 				game.PlayerPlay <- newplay
 			}
